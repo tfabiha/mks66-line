481645d745adb789022e80c1c90a8c073c92c163
tfabiha <tfabiha@stuy.edu>
2019-02-07 12:02:13 -0500

    o7 done one left

--- draw.py
+++ draw.py
@@ -2,13 +2,19 @@ from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
 
-    slope = (y1 - y0) * 1.0 / (x1 - x0)
+    if (x1 - x0) == 0:
+        slope = 1000000
+    else:
+        slope = (y1 - y0) * 1.0 / (x1 - x0)
+
     print( slope )
     
     if 0 <= slope <= 1:
         draw_o1( x0, y0, x1, y1, screen, color )
-    elif slope < 1:
+    elif slope > 1:
         draw_o2( x0, y0, x1, y1, screen, color )
+    elif -1 <= slope <= 0:
+        draw_o8( x0, y0, x1, y1, screen, color )
         
 def draw_o1( x0, y0, x1, y1, screen, color ):
     x = x0
@@ -20,8 +26,6 @@ def draw_o1( x0, y0, x1, y1, screen, color ):
 
     while x <= x1:
         plot(screen, color, x, y)
-
-        #print( d )
         
         if d > 0:
             y += 1
@@ -30,7 +34,7 @@ def draw_o1( x0, y0, x1, y1, screen, color ):
         x += 1
         d += 2*A
 
-def draw_o2( x0, y0, x1, y2, screen, color ):
+def draw_o2( x0, y0, x1, y1, screen, color ):
     x = x0
     y = y0
 
@@ -48,3 +52,20 @@ def draw_o2( x0, y0, x1, y2, screen, color ):
         y += 1
         d += 2*B
             
+def draw_o8( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+
+    A = y1 - y0
+    B = x0 - x1 # - ( x1 - x0 )
+    d = 2*A - B
+
+    while x <= x1:
+        plot(screen, color, x, y)
+        
+        if d < 0:
+            y -=1
+            d -= 2*B
+
+        x += 1
+        d += 2*A
--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line( 50, 50, 170, 300, screen, color)
+draw_line( 300, 300, 500, 700, screen, color)
+draw_line( 300, 300, 500, 600, screen, color)
+draw_line( 300, 300, 500, 500, screen, color)
+draw_line( 300, 300, 500, 400, screen, color)
+draw_line( 300, 300, 500, 300, screen, color)
+draw_line( 300, 300, 500, 200, screen, color)
+draw_line( 300, 300, 500, 100, screen, color)
 
 display(screen)
 save_extension(screen, 'img.png')
