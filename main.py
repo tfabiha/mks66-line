004b5896a7c8658f12547bb944b3d4d258c27859
tfabiha <tfabiha@stuy.edu>
2019-02-10 22:48:37 -0500

    adding finished hw

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
+draw_line( 300, 300, 100, 700, screen, color)
+draw_line( 300, 300, 100, 600, screen, color)
+draw_line( 300, 300, 100, 500, screen, color)
+draw_line( 300, 300, 100, 400, screen, color)
+draw_line( 300, 300, 100, 300, screen, color)
 draw_line( 300, 300, 100, 200, screen, color)
+draw_line( 300, 300, 100, 100, screen, color)
+draw_line( 300, 300, 100, -100, screen, color)
+
+
+
 draw_line( 300, 300, 300, 700, screen, color)
 draw_line( 300, 300, 500, 700, screen, color)
 draw_line( 300, 300, 500, 600, screen, color)
